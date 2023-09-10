ba3371d2b01647bed4400579f2e8da5cdcdfaeef
Dreecool <114856837+Dreecool@users.noreply.github.com>
2023-09-10 12:42:01 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,50 +16,39 @@ mongoose.connect("mongodb+srv://francesdonz23:password1234@auth.34tuwf2.mongodb.
 
 })
 
-app.use(cors(
-  {
-    origin: ["https://chat-apps-yr71.vercel.app/"],
+app.use(
+  cors({
+    origin: ["https://chat-apps-nine.vercel.app"], // Update with your frontend URL
     methods: ["POST", "GET", "DELETE", "PUT"],
-    credentials: true
-  }
-));
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post("/Register", async (req, res) => {
-
+app.post("/api/Register", async (req, res) => {
   const full_name = req.body.full_name;
   const email_address = req.body.email_address;
   const password = req.body.password;
 
- 
   try {
-
-
     const Reg = new Register({
       full_name: full_name,
       email_address: email_address,
-      password: password
-
-
+      password: password,
     });
 
     await Reg.save();
     res.status(200).json({ message: "Registration successful" });
-
-
   } catch (err) {
-
     console.error(err);
     res.status(500).json({ error: "Registration failed" });
-
-
   }
 });
 
-app.post("/LoginUser", async (req, res) => {
+app.post("/api/LoginUser", async (req, res) => {
   const email_address = req.body.email_address;
 
   try {
@@ -69,56 +58,38 @@ app.post("/LoginUser", async (req, res) => {
       const userId = existingUser._id;
       const token = jwt.sign({ userId }, secretKey, { expiresIn: "1d" });
 
-      res.cookie('token', token, { httpOnly: true, maxAge: 3600000 });
-      res.status(200).json({ message: 'Login successful' }); 
+      res.cookie("token", token, { httpOnly: true, maxAge: 3600000 });
+      res.status(200).json({ message: "Login successful" });
     } else {
-      res.status(401).json({ message: 'Invalid credentials' });
+      res.status(401).json({ message: "Invalid credentials" });
     }
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: 'Login failed' }); 
+    res.status(500).json({ message: "Login failed" });
   }
 });
 
-
 const verifyUser = (req, res, next) => {
+  const token = req.cookies.token;
 
-  const token  = req.cookies.token;
-
-  if(!token) {
-
-    return res.json({ message: 'Unauthorized' });
-
+  if (!token) {
+    return res.json({ message: "Unauthorized" });
   } else {
-
     jwt.verify(token, secretKey, (err, decoded) => {
-
-      if(err) {
-
-        return res.json({message: "Token is not valid"})
-
+      if (err) {
+        return res.json({ message: "Token is not valid" });
       } else {
-
         req.userId = decoded.userId;
-        next()
-
+        next();
       }
-    })
+    });
   }
+};
 
-}
-
-app.get("/LoggedIn", verifyUser, (req, res) => {
-  
-  return res.json({Message: "Authorized"});
-
-})
-
-
-
+app.get("/api/LoggedIn", verifyUser, (req, res) => {
+  return res.json({ Message: "Authorized" });
+});
 
 app.listen(PORT, () => {
-
-  console.log("Port is running")
-
-})
+  console.log("Server is running on port " + PORT);
+});
